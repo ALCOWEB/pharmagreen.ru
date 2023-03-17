cfeac5172be0ad32466fa1f874af4abe61f932b7
KMochalov <kmochalov@srt-llc.ru>
2023-03-17 11:31:06 +0300

    add some vue changes

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,30 +1,81 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+      <form>
+        <h4>Create post</h4>
+        <input v-bind:value="title" class="input" type="text" placeholder="название">
+        <input v-bind:value="body"  class="input" type="text" placeholder="описание">
+        <button class="btn" @click="createPost">create</button>
+      </form>
+    <div class="post" v-for="post in posts">
+      <div><strong>Название:</strong> {{ post.title }} </div>
+      <div><strong>Описание</strong> {{ post.body }}</div>
+    </div>
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style>
+  .app {
+    padding: 20px;
+  }
 
-nav {
-  padding: 30px;
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  .post {
+    padding: 15px;
+    border: 2px solid teal;
+    margin-top: 15px;
+  }
 
-    &.router-link-exact-active {
-      color: #42b983;
+  .input {
+    width: 100%;
+    border: 2px solid teal;
+    padding: 10px 15px;
+    margin-top: 15px;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .btn {
+    align-self: flex-end;
+    margin-top: 15px;
+    padding: 10px 15px;
+    background: none;
+    color: teal;
+    border: 1px solid teal;
+  }
+</style>
+
+<script>
+  export default {
+    data() {
+      return {
+        posts: [
+          {
+            id: 1,
+            title: 'Title js',
+            body: 'body of post js'
+          },
+          {
+            id: 2,
+            title: 'Title php',
+            body: 'body of post php'
+          },
+        ],
+        title: '',
+        body: ''
+      }
+    },
+    methods: {
+      createPost() {
+
+      }
     }
   }
-}
-</style>
+</script>
