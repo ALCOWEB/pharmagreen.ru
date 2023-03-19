0914813aa28118dcfe8d0ae4ae331a755619809b
KMochalov <KMochalov@srt-llc.ru>
2023-03-19 21:14:48 +0300

    some changes vue 3

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="app">
     <h1>Posts page</h1>
-    <my-button
-      @click="showDialog"
-      style="margin: 15px 0"
-    >Create post</my-button>
-    <my-button
-        @click="fetchPosts"
-        style="margin: 15px 0"
-    >Получить постddы</my-button>
+    <my-input v-model="searchQuery"/>
+    <div class="app_btns">
+      <my-button
+          @click="showDialog"
+      >Create posts</my-button>
+      <my-select
+          v-model="selectedSort"
+          :options="sortOptions"
+        />
+    </div>
     <my-dialog v-model:show="dialogVisible">
       <post-form
           @create="createPost"
       />
     </my-dialog>
     <post-list
-        :posts="posts"
+        :posts="sortedAndSearchedPosts"
         @remove="removePost"
+        v-if="!postsLoading"
     />
+    <div v-else>идёт загрузка</div>
   </div>
 </template>
 
@@ -25,7 +29,11 @@
   .app {
     padding: 20px;
   }
-
+  .app_btns {
+    margin: 15px 0;
+    display: flex;
+    justify-content: space-between;
+  }
   * {
     margin: 0;
     padding: 0;
@@ -41,8 +49,13 @@ import PostForm from "@/components/PostForm.vue";
 import MyButton from "@/components/UI/MyButton.vue";
 import MyDialog from "@/components/UI/MyDialog.vue";
 import axios from 'axios';
+import {onMounted} from "vue";
+import MySelect from "@/components/UI/MySelect.vue";
+import MyInput from "@/components/UI/MyInput.vue";
 export default {
     components: {
+      MyInput,
+      MySelect,
       MyDialog,
       MyButton,
       PostForm, PostList
@@ -50,7 +63,20 @@ export default {
     data() {
       return {
         posts: [],
-        dialogVisible: false
+        dialogVisible: false,
+        postsLoading: false,
+        selectedSort: '',
+        searchQuery: '',
+        sortOptions: [
+          {
+            value: 'title',
+            name: 'По названию'
+          },
+          {
+            value: 'body',
+            name: 'по описанию'
+          }
+        ]
       }
     },
     methods: {
@@ -66,12 +92,31 @@ export default {
       },
       async fetchPosts() {
         try {
-          const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
-          this.posts = response.data;
+          this.postsLoading = true;
+            const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+            this.posts = response.data;
         } catch (e) {
           alert("error")
+        } finally {
+          this.postsLoading = false;
         }
       }
+    },
+    mounted() {
+      this.fetchPosts();
+    },
+    computed: {
+      sortedPosts() {
+       return [...this.posts].sort((a, b) => {
+         return a[this.selectedSort]?.localeCompare(b[this.selectedSort]);
+       })
+      },
+      sortedAndSearchedPosts() {
+        return this.sortedPosts.filter(post => post.title.includes(this.searchQuery));
+      }
+    },
+    watch: {
+
     }
   }
 </script>
--- front/src/components/PostList.vue
+++ front/src/components/PostList.vue
@@ -1,12 +1,15 @@
 <template>
   <div v-if="posts.length > 0">
     <h3>Список постов</h3>
-    <post-item class="post"
-         v-for="post in posts"
-         :post="post"
-         :key="post.id"
-         @remove="$emit('remove', post)"
-    />
+    <transition-group name="postList">
+      <post-item class="post"
+                 v-for="post in posts"
+                 :post="post"
+                 :key="post.id"
+                 @remove="$emit('remove', post)"
+      />
+    </transition-group>
+
   </div>
   <h2 v-else style="color: red">Список постов пуст</h2>
 </template>
@@ -26,5 +29,21 @@ export default {
 </script>
 
 <style scoped>
+  .postList-item {
+    display: inline-block;
+    margin-right: 10px;
+  }
+  .postList-enter-active,
+  .postList-leave-active {
+    transition: all 0.4s ease;
+  }
+  .postList-enter-from,
+  .postList-leave-to {
+    opacity: 0;
+    transform: translateX(130px);
+  }
+  .postList-move {
+    transition: transform 0.4s ease;
+  }
 
 </style>
